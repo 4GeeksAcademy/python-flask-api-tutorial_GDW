a9a1bca340452a212dbb278009a3260d8d3979ff
Guy David Whittaker <105855731+gdwhittaker94@users.noreply.github.com>
2023-10-09 15:23:22 +0000

    Gone to end of exercises

--- src/app.py
+++ src/app.py
@@ -1,9 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
+todos = [
+  {"label": "My first task", "done": False},
+  {"label": "My second task", "done": False},
+]
 
+# GET
+@app.route('/todos', methods=['GET'])
+def hello_world():
+  print('Testing')
+  json_data = jsonify(todos)
+  return json_data
 
+# POST
+@app.route('/todos', methods=['POST'])
+def add_new_todo(): 
+  request_body = request.get_json(silent=True)
+  if request_body is None: 
+    return 'Invalid JSON in request body', 400
+  todos.append(request_body)
+  return jsonify(todos), 200
 
+# DELETE
+@app.route('/todos/<int:todo_id>', methods=['DELETE'])
+def delete_todo(todo_id): 
+  del todos[todo_id]
+  return jsonify(todos), 200
 
 # END
 if __name__ == '__main__':
